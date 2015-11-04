5af086625369fa5595c66a95ab726f4ba63b8242
李韧 <lr_5146@163.com>
2015-11-04 14:19:35 +0800

    Initial Commit

---
+++ LCGraffitiImageView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCGraffitiImageView
+//
+//  Created by lazy on 15/11/4.
+//  Copyright © 2015年 lazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
